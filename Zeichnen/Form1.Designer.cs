4df9a10fc1db7eee493a2d273c755aead686fa96
MajaWefers <papillon87@web.de>
2017-02-26 11:50:09 +0100

    Timer

--- Zeichnen/Form1.Designer.cs
+++ Zeichnen/Form1.Designer.cs
@@ -28,21 +28,43 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.Zaehler = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // Zaehler
+            // 
+            this.Zaehler.AutoSize = true;
+            this.Zaehler.Location = new System.Drawing.Point(448, 9);
+            this.Zaehler.Name = "Zaehler";
+            this.Zaehler.Size = new System.Drawing.Size(0, 13);
+            this.Zaehler.TabIndex = 0;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(360, 262);
+            this.ClientSize = new System.Drawing.Size(484, 462);
+            this.Controls.Add(this.Zaehler);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Label Zaehler;
     }
 }
 
--- Zeichnen/Form1.cs
+++ Zeichnen/Form1.cs
@@ -5,11 +5,17 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace Zeichnen
 {
+    /*public class Thread_1
+    {
+        System.Threading.Thread thdWorker = new Thread(new ThreadStart(Form1.Form1_Paint()));
+    }*/
+
+
     public partial class Form1 : Form
     {
         public Form1()
@@ -17,121 +23,151 @@ namespace Zeichnen
             InitializeComponent();
         }
 
-        static System.Windows.Forms.Timer zeit = new System.Windows.Forms.Timer();
+        //static System.Windows.Forms.Timer zeit = new System.Windows.Forms.Timer();
 
         static int grid = 10;
-        static int abstand = 20;
+        static int feldgroesse = 20;
 
-        int[,] feld = new int[grid, grid];
+        static int[,] gen_akt = new int[grid, grid];
+        
+        static int p_x = feldgroesse;
+        static int p_y = feldgroesse;
+        static int start = 1;
 
-        int p_x = abstand;
-        int p_y = abstand;
-
-        zeit.Tick += new EventHandler(TimerEventProcessor);
-        zeit.Interval = 2000;
-        //zeit.Elapsed += OnTick;
-        zeit.Start();
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
-            //Update(feld);
-
-            //TODO: Time-Loop
-        
-
-            Start_Stellung(feld);
-
-            for (int x = 0; x < feld.GetLength(0); x++)
+            if ( start == 1 )
             {
-                p_x = 0;
+                Start_Stellung(gen_akt);
+                start++;
+            }
+            
+            while (1 > 0)
+            {
 
-                for (int y = 0; y < feld.GetLength(1); y++)
+                //Zurücksetzen
+                int[,] gen_neu = new int[grid, grid];
+                p_y = 0;
+
+                //Zeichnen anhand der Werte in gen_akt
+                for (int x = 0; x < gen_akt.GetLength(0); x++)
                 {
+                    p_x = 0;  //Zurücksetzen
 
-                    /*if (y % 2 == 0)
+                    p_y += feldgroesse; //Position y
+
+                    for (int y = 0; y < gen_akt.GetLength(1); y++)
                     {
-                        feld[x, y] = 0;
-                    }
-
-                    if (y % 2 == 1)
-                    {
-                        feld[x, y] = 1;
-                    }*/
-
-                    p_x += abstand;
-
-                    if (feld[x, y] == 1)
-                    {
-                        e.Graphics.FillRectangle(Brushes.Green, p_x, p_y, 20, 20);
-                        //p_y += 20;
-                    }
-
-
-                    if (feld[x, y] == 0)
-                    {
-                        e.Graphics.DrawRectangle(Pens.Blue, p_x, p_y, 20, 20);
-                        ///p_y += 20;
+                        p_x += feldgroesse; //Position x
+                        
+                        if (gen_akt[x, y] == 1)
+                        {
+                            e.Graphics.FillRectangle(Brushes.Blue, p_x, p_y, feldgroesse, feldgroesse);
+                        }
+                        
+                        if (gen_akt[x, y] == 0)
+                        {
+                            e.Graphics.DrawRectangle(Pens.Blue, p_x, p_y, feldgroesse, feldgroesse);
+                        }
                     }
                 }
 
-                p_y += abstand;
-            }
+                Naechste_Gen(gen_akt, gen_neu);
+                gen_akt = gen_neu;
 
-            //Update(feld);
-            //Ende Loop
-                
+                Thread.Sleep(200);
+                Form1.ActiveForm.Refresh();
+
+                //Form1.ActiveForm.Update();
+                //Form1.ActiveForm.Invalidate();
+            }//while
+
+        }//Form1_Paint
+        
+        public static void Start_Stellung(int[,] feld)
+        {
+            feld[2, 2] = 1;
+            feld[2, 3] = 1;
+            feld[2, 4] = 1;
+            feld[1, 4] = 1;
+            feld[0, 3] = 1;
         }
 
-        private void OnTick(object source, ElapsedEventArgs e)
+        public static void Naechste_Gen(int[,] gen_akt, int[,] gen_neu)
         {
-
-        }
-
-        static void Start_Stellung(int[,] feld)
-        {
-            feld[5, 4] = 1;
-            feld[5, 5] = 1;
-            feld[5, 6] = 1;
-        }
-
-        static void Update(int[,] feld)
-        {
-            for (int x = 1; x < feld.GetLength(0); x++)
+            for (int r = 0; r < gen_akt.GetLength(0); r++)
             {
-                for (int y = 1; y < feld.GetLength(1); y++)
+                for (int c = 0; c < gen_akt.GetLength(1); c++)
                 {
-                    if (feld[x, y] == 1)
-                    {
-                        
-                        if (   feld[x - 1, y - 1]   + feld[x - 1, y]    + feld[x - 1, y + 1]
-                             + feld[x, y - 1]                           + feld[x, y + 1]
-                             + feld[x + 1, y - 1]   + feld[x + 1, y]    + feld[x + 1, y + 1]
-                             < 2)
-                        {
-                            feld[x, y] = 0;
-                        }
+                    int rO = r;
+                    int rU = r;
+                    int cL = c;
+                    int cR = c;
 
-                        if (   feld[x - 1, y - 1]   + feld[x - 1, y] + feld[x - 1, y + 1]
-                             + feld[x, y - 1]                        + feld[x, y + 1]
-                             + feld[x + 1, y - 1]   + feld[x + 1, y] + feld[x + 1, y + 1]
+                    //Sonderbedingung für Ränder
+                    if (r == 0)
+                    {
+                        rO = gen_akt.GetLength(0);
+                    }
+                    if (r == gen_akt.GetLength(0) - 1)
+                    {
+                        rU = -1;
+                    }
+                    if (c == 0)
+                    {
+                        cL = gen_akt.GetLength(1);
+                    }
+                    if (c == gen_akt.GetLength(1) - 1)
+                    {
+                        cR = -1;
+                    }
+
+                    //Leben und sterben
+                    if (gen_akt[r, c] == 1)
+                    {
+                        //sterben
+                        if (   gen_akt[rO - 1, cL - 1] + gen_akt[rO - 1, c] + gen_akt[rO - 1, cR + 1]
+                             + gen_akt[r, cL - 1]                           + gen_akt[r, cR + 1]
+                             + gen_akt[rU + 1, cL - 1] + gen_akt[rU + 1, c] + gen_akt[rU + 1, cR + 1]
+                             < 2
+                             ||
+                               gen_akt[rO - 1, cL - 1] + gen_akt[rO - 1, c] + gen_akt[rO - 1, cR + 1]
+                             + gen_akt[r, cL - 1]                           + gen_akt[r, cR + 1]
+                             + gen_akt[rU + 1, cL - 1] + gen_akt[rU + 1, c] + gen_akt[rU + 1, cR + 1]
                              > 3)
                         {
-                            feld[x, y] = 0;
+                            gen_neu[r, c] = 0;
+                        }
+
+                        //weiter leben
+                        else
+                        {
+                            gen_neu[r, c] = 1;
                         }
                     }
 
-                    if (feld[x, y] == 0)
+                    if (gen_akt[r, c] == 0)
                     {
-                        if (   feld[x - 1, y - 1]   + feld[x - 1, y]    + feld[x - 1, y + 1]
-                             + feld[x, y - 1]       + feld[x, y]        + feld[x, y + 1]
-                             + feld[x + 1, y - 1]   + feld[x + 1, y]    + feld[x + 1, y + 1]
+                        //neues leben
+                        if (   gen_akt[rO - 1, cL - 1] + gen_akt[rO - 1, c] + gen_akt[rO - 1, cR + 1]
+                             + gen_akt[r, cL - 1]                           + gen_akt[r, cR + 1]
+                             + gen_akt[rU + 1, cL - 1] + gen_akt[rU + 1, c] + gen_akt[rU + 1, cR + 1]
                              == 3)
                         {
-                            feld[x, y] = 1;
+                            gen_neu[r, c] = 1;
                         }
                     }
-                }//for y
-            }   //for x
-        }       //Update
+                }//y
+            }//x
+
+        }//Naechste_Gen
+
+        /*static void Feld_zeichnen(int[,] feld,int feldgroesse)
+        {
+            int p_x = feldgroesse;
+            int p_y = feldgroesse;
+        }*/
+
     }
 }
